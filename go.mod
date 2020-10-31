dc09798b249d705cc5744ea66d53f1df3dcbe89f
Archie Brand-Williamson <archie.brand@amido.com>
2020-10-31 11:00:27 +0000

    Use echo framework

--- go.mod
+++ go.mod
@@ -2,4 +2,11 @@ module todo
 
 go 1.15
 
-require github.com/gorilla/mux v1.8.0
+require (
+	github.com/labstack/echo/v4 v4.1.17
+	github.com/mattn/go-colorable v0.1.8 // indirect
+	golang.org/x/crypto v0.0.0-20201016220609-9e8e0b390897 // indirect
+	golang.org/x/net v0.0.0-20201027133719-8eef5233e2a1 // indirect
+	golang.org/x/sys v0.0.0-20201028094953-708e7fb298ac // indirect
+	golang.org/x/text v0.3.4 // indirect
+)
--- main.go
+++ main.go
@@ -1,55 +1,60 @@
 package main
 
 import (
-	"fmt"
-	"strconv"
-
 	"net/http"
-	"os"
-
+	"strconv"
 	"todo/internal/memorydb"
+	"todo/internal/models"
 
-	"github.com/gorilla/mux"
+	"github.com/labstack/echo/v4"
 )
 
 var todoDB *memorydb.TodoDB
 
 func main() {
-
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8000" //localhost
-	}
-
+	e := echo.New()
+	setupRouting(e)
 	initialiseDB()
-	router := setupRouter()
+	e.Logger.Fatal(e.Start(":8000"))
+}
 
-	err := http.ListenAndServe(":"+port, router) //Launch the app, visit localhost:8000
-	if err != nil {
-		fmt.Print(err)
+func setupRouting(e *echo.Echo) {
+	e.GET("/todo/:id", getTodo)
+	e.POST("/todo", saveTodo)
+	e.PUT("/todo/:id", updateTodo)
+	e.DELETE("/todo/:id", deleteTodo)
+}
+
+func getTodo(c echo.Context) error {
+	id, _ := strconv.Atoi(c.Param("id"))
+	t, ok := todoDB.Read(id)
+	if !ok {
+		return c.JSON(http.StatusNotFound, t)
 	}
+	return c.JSON(http.StatusCreated, t)
+}
+
+func saveTodo(c echo.Context) error {
+	t := new(models.Todo)
+	if err := c.Bind(t); err != nil {
+		return err
+	}
+	return c.JSON(http.StatusCreated, t)
+}
+
+func updateTodo(c echo.Context) error {
+	// User ID from path `todo/:id`
+	id := c.Param("id")
+	return c.String(http.StatusOK, id)
+}
+
+func deleteTodo(c echo.Context) error {
+	// User ID from path `todo/:id`
+	id := c.Param("id")
+	return c.String(http.StatusOK, id)
 }
 
 func initialiseDB() {
 	todoDB = new(memorydb.TodoDB)
 	todoDB.Initialise()
-}
-
-func setupRouter() *mux.Router {
-	r := mux.NewRouter()
-
-	r.HandleFunc("/todo/{id}", func(w http.ResponseWriter, r *http.Request) {
-		vars := mux.Vars(r)
-		id, err := strconv.Atoi(vars["id"])
-		if err != nil {
-			fmt.Printf("GET: Invalid URL /todo/%s", vars["id"])
-			w.WriteHeader(404)
-			fmt.Fprintf(w, "404 GET: todo ID: %s\n", id)
-		}
-		if todo, ok := todoDB.Read(int(id)); ok == true {
-			json, _ := todo.JSON()
-			w.Write(json)
-		}
-	})
-	return r
 }
